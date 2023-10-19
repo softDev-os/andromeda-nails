b85805b40c4f95b32941478c30c35bd4837ac6d4
softDev-os <softdev.iso@gmail.com>
2023-10-18 21:43:07 -0500

    Category

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,9 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Acrilicas',
+	'Polygel',
+	'Comercial',
+	'Semipermanente',
+	'Pedicure',
+	'Spa'
 ] as const
