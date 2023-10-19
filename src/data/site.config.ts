279fc541b8d3c283d912de9ee018fc00fd49f94c
softDev-os <softdev.iso@gmail.com>
2023-10-18 20:06:15 -0500

    SEO

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -8,10 +8,12 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG',
-	title: 'Astro Theme OpenBlog',
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post' // Message to share a post on social media
+	author: 'Juan Fontalvo (softDev) - Web Develoment',
+	title: 'Andromeda Nails',
+	description:
+		'Explora nuestra colección de diseños de uñas acrílicas, techgel y comerciales. Encuentra inspiración en nuestra variada galería de imágenes de manicuras y tendencias de belleza.',
+	lang: 'es_CO',
+	ogLocale: 'es_CO',
+	shareMessage:
+		'Explora nuestra colección de diseños de uñas acrílicas, techgel y comerciales. Encuentra inspiración en nuestra variada galería de imágenes de manicuras y tendencias de belleza.' // Message to share a post on social media
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				white: '#f8f9fa'
+				white: '#eff4ff'
 			},
 			fontFamily: {
 				body: ['Manrope', ...defaultTheme.fontFamily.sans]
