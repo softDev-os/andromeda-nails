865e708eee23c6be6e6041acaa4f6e0251381ab1
softDev-os <softdev.iso@gmail.com>
2023-10-26 12:29:00 -0500

    SEO

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,7 +11,7 @@ export const siteConfig: SiteConfig = {
 	author: 'Juan Fontalvo (softDev) - Web Develoment',
 	title: 'Andromeda Nails',
 	description:
-		'Explora nuestra colección de diseños de uñas acrílicas, techgel y comerciales. Encuentra inspiración en nuestra variada galería de imágenes de manicuras y tendencias de belleza.',
+		'Explora variados diseños de uñas acrílicas, techgel y comerciales. Inspírate en nuestra galería de tendencias y manicuras.',
 	lang: 'es_CO',
 	ogLocale: 'es_CO',
 	shareMessage:
